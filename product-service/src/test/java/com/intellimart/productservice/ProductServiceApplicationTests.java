7dd1da5992b57f769973cb280f9cee0d379824ff
Abdul Bari <bariabdul11ab@google.com>
2025-07-28 21:42:41 +0530

    Day-45

--- product-service/src/test/java/com/intellimart/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/intellimart/productservice/ProductServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.intellimart.productservice;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = Replace.NONE) // Add this line
 class ProductServiceApplicationTests {
 
 	@Test

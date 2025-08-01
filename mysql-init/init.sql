d83678c2f6700e7769b58f83997bd5864b5a893d
Abdul Bari <bariabdul11ab@google.com>
2025-08-02 00:13:03 +0530

    order-service is not fixed but eureka is registering

---
+++ mysql-init/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS intelimart_auth_db;
+CREATE DATABASE IF NOT EXISTS intelimart_product_db;
+CREATE DATABASE IF NOT EXISTS intelimart_cart_db;
+CREATE DATABASE IF NOT EXISTS intelimart_order_db;
